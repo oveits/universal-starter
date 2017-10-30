980bf22f74ebd7193796ebb0ffcb754738026f24
Cloud User <centos@ip-172-31-21-180.us-east-2.compute.internal>
2017-10-30 10:51:10 +0000

    Added HTML template with innerHTML and HTTP GET in the blog component
    However, server-side rendering does not work correctly. Instead we get as log in the server terminal:
    ERROR Error: not implemented
        at Parse5DomAdapter.getCookie (/app/dist/server.js:39001:68)
        at CookieXSRFStrategy.configureRequest (/app/dist/server.js:43414:119)
        at XHRBackend.createConnection (/app/dist/server.js:43463:28)
        at httpRequest (/app/dist/server.js:43871:20)
        at Http.request (/app/dist/server.js:43981:34)
        at Http.get (/app/dist/server.js:43995:21)
        at e.X+Mx.e.ngOnInit (/app/dist/server.js:82833:8159)
        at checkAndUpdateDirectiveInline (/app/dist/server.js:11698:19)
        at checkAndUpdateNodeInline (/app/dist/server.js:13196:20)
        at checkAndUpdateNode (/app/dist/server.js:13139:16)

--- cli/src/app/blog/blog.module.ts
+++ cli/src/app/blog/blog.module.ts
@@ -1,16 +1,40 @@
 // src/app/blog/blog.module.ts
-import {NgModule, Component} from '@angular/core'
+import {NgModule, Component, OnInit} from '@angular/core'
 import {RouterModule} from '@angular/router'
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'blog-view',
   templateUrl: './blog.component.html'
 })
-export class BlogComponent {}
+
+export class BlogComponent implements OnInit {
+
+  title : String = "Loading..."
+  content : String = "Loading..."
+
+  constructor(private _http: Http) {}
+
+  ngOnInit() {
+     this._http.get('https://public-api.wordpress.com/rest/v1.1/sites/oliverveits.wordpress.com/posts/3078')
+                .map((res: Response) => res.json())
+                 .subscribe(data => {
+                        this.title = data.title;
+                        this.content = data.content;
+                        console.log(data);
+                });
+  }
+  
+}
+
+import { HttpModule }    from '@angular/http';
 
 @NgModule({
   declarations: [BlogComponent],
   imports: [
+    HttpModule,
     RouterModule.forChild([
       { path: '', component: BlogComponent, pathMatch: 'full'}
     ])
